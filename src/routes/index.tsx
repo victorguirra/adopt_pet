c10339eb7f3babb07c084ee1bf5fb694494c20e7
victorguirra <victor.hugo.guirra@gmail.com>
2021-05-05 20:56:27 -0300

    import new page(main)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../components/Layout';
 
 import Home from '../pages/Home';
+import Main from '../pages/Main';
 import Profile from '../pages/Profile';
 import Settings from '../pages/Settings';
 
@@ -13,6 +14,7 @@ function Routes(){
             <Layout>
                 <Switch>
                     <Route exact path="/" component={ Home } />
+                    <Route exact path="/main" component={ Main } />
                     <Route exact path="/profile" component={ Profile } />
                     <Route exact path="/settings" component={ Settings } />
                 </Switch>
