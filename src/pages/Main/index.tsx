a0a29cb9442e8c35d428107d5e0dc113ae23ac7e
victorguirra <victor.hugo.guirra@gmail.com>
2021-05-05 20:56:03 -0300

    create main page

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+function Main(){
+    return(
+        <Container>
+            hello main
+        </Container>
+    )
+}
+
+export default Main;
