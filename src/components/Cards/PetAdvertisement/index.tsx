d5114ca0210ecc86d6448ab80b96cc7d56cfb2ef
victorguirra <victor.hugo.guirra@gmail.com>
2021-05-06 22:12:15 -0300

    create pet advertisement card

---
+++ src/components/Cards/PetAdvertisement/index.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { Container, Header, Body, Footer } from './styles';
+
+import Carousel from '../../Carousel';
+
+import { FaWhatsapp } from 'react-icons/fa';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { RiDislikeLine } from 'react-icons/ri';
+import { IoPersonAddSharp, IoPersonRemoveSharp } from 'react-icons/io5';
+
+interface IPetAdvertisementProps {
+    data: IDataProps;
+}
+
+interface IDataProps {
+    authorName: string;
+    authorImage: string;
+    announcementDate: string;
+    isLiked: boolean;
+    inYourContacts: boolean;
+    petInfos: {
+        name: string;
+        desctiption: string;
+        images: IPetAdvertimentImagesProps[];
+    };
+}
+
+interface IPetAdvertimentImagesProps {
+    source: string;
+}
+
+function PetAdvertisement({ data } : IPetAdvertisementProps){
+    const [ isLiked, setIsLiked ] = useState<boolean>(data.isLiked);
+    const [ inYourContacts, setInYourContacts ] = useState<boolean>(data.inYourContacts);
+    
+    return(
+        <Container>
+            <Header>
+                <main>
+                    <img src={ data.authorImage } alt={ data.authorName } />
+                    
+                    <div>
+                        <span>{ data.authorName }</span>
+                        <span>{ data.announcementDate }</span>
+                    </div>
+                </main>
+
+                <button onClick={ () => setInYourContacts(!inYourContacts) }>
+                    { inYourContacts
+                        ? <IoPersonRemoveSharp size={ 25 } color="#E33D3D" />
+                        : <IoPersonAddSharp size={ 25 } color="#8257E5" />
+                    }
+                </button>
+            </Header>
+
+            <Body>
+                <span>{ data.petInfos.name }</span>
+
+                <p>{ data.petInfos.desctiption }</p>
+
+                <Carousel 
+                    petName={ data.petInfos.name } 
+                    petImages={ data.petInfos.images }
+                />
+            </Body>
+
+            <Footer>
+                <a href="http://www.google.com.br" target="blank">
+                    <FaWhatsapp size={ 25 } color="#FFF" />
+                    Entrar em contato
+                </a>
+                <button 
+                    onClick={ () => setIsLiked(!isLiked) }
+                    className={ isLiked ? 'isLiked' : '' }
+                >
+                    { isLiked
+                        ? <RiDislikeLine size={ 25 } color="#FFF" />
+                        : <AiOutlineHeart size={ 25 } color="#FFF" />
+                    }
+                </button>
+            </Footer>
+        </Container>
+    )
+}
+
+export default PetAdvertisement;
---
+++ src/components/Cards/PetAdvertisement/styles.ts
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+    width:25rem;
+    padding:1rem;
+
+    border-radius:.8rem;
+    background-color:${({ theme }) => theme.colors.white };
+`;
+
+export const Header = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+
+    main {
+        display:flex;
+        align-items:center;
+    
+        img {
+            width:5rem;
+            border-radius:50%;
+        }
+
+        div {
+            display:flex;
+            flex-direction:column;
+
+            margin-left:1.2rem;
+
+            span + span {
+                color:${({ theme }) => theme.colors.gray_800 };
+                margin-top:.4rem;
+            }
+        }
+    }
+
+    button {
+        background:transparent;
+    }
+`;
+
+export const Body = styled.main`
+    margin-top:2rem;
+    
+    > span {
+        font-weight:bold;
+    }
+
+    p {
+        color:${({ theme }) => theme.colors.gray_800 };
+        margin-top:.7rem;
+        margin-bottom:2rem;
+    }
+`;
+
+export const Footer = styled.footer`
+    margin-top:2rem;
+    
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+
+    button, a {
+        height:3.4rem;
+        border-radius:.4rem;
+
+        display:flex;
+        align-items:center;
+        justify-content:center;
+    }
+
+    a {
+        width:75%;
+        gap:.8rem;
+
+        font-weight:bold;
+        font-size:.9rem;
+
+        text-decoration:none;
+
+        color:${({ theme }) => theme.colors.white };
+        background-color:${({ theme }) => theme.colors.green };
+    }
+
+    button {
+        width:20%;
+        background-color:${({ theme }) => theme.colors.purple_500 };
+
+        &.isLiked {
+            background-color:${({ theme }) => theme.colors.red };
+        }
+    }
+`;
