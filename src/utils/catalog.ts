8e9e8b50dc4bd2480e3fa4950318b50c0ec471a3
victorguirra <victor.hugo.guirra@gmail.com>
2021-05-06 22:14:10 -0300

    create catalog file for simulate api

---
+++ src/utils/catalog.ts
@@ -0,0 +1,23 @@
+const catalog = [
+    {
+        authorName:'Victor Guirra',
+        authorImage: 'https://avatars.githubusercontent.com/u/63051439?v=4',
+        announcementDate: '01/07/2020',
+        isLiked: false,
+        inYourContacts: false,
+
+        petInfos: {
+            name: 'Toby',
+            desctiption: 'Toby é um cachorro muito bonzinho, educado e obediente, um verdadeiro amigão...',
+
+            images: [
+                { source: 'https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZG9nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
+                { source: 'https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
+                { source: 'https://images.unsplash.com/photo-1544568100-847a948585b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGRvZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' },
+                { source: 'https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGRvZ3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
+            ]
+        }
+    },
+]
+
+export default catalog;
