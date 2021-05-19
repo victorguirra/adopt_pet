2e01c21c937841a426f65ea5bf5983bfc47b05e7
victorguirra <victor.hugo.guirra@gmail.com>
2021-05-19 20:26:32 -0300

    create profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
 import { Container } from './styles';
 
+import { FaWhatsapp } from 'react-icons/fa';  
+import { AiOutlineInstagram } from 'react-icons/ai';
+
+interface IProfileData {
+    key: string;
+    title: string;
+    amount: number;
+}
+
 function Profile(){
+    const imageURL = "https://avatars.githubusercontent.com/u/63051439?v=4";
+    
+    const profileData: IProfileData[] = [
+        { key: 'Favorite Pets', title: 'Pets Favoritados', amount: 79 },
+        { key: 'Registered Pets', title: 'Pets Cadastrados', amount: 12 },
+        { key: 'Contacts', title: 'Contatos Adicionados', amount: 34 },
+    ]
+
     return(
         <Container>
-            <h3>Profile Page</h3>
+            <h1>Meu Perfil</h1>
+            
+            <main>
+                <img src={ imageURL } alt="Victor Guirra" />
+                <p>Victor Guirra</p>
+                <span>Riacho Fundo I - Brasília, DF</span>
+                <div className="medias">
+                    <p>
+                        <FaWhatsapp size={ 25 } color="#000" />
+                        (61) 98562-2749
+                    </p>
+                    <p>|</p>
+                    <p>
+                        <AiOutlineInstagram size={ 25 } color="#000" />
+                        _guirra
+                    </p>
+                </div>
+
+                <footer>
+                    { profileData.map(item => (
+                        <div key={ item.key }>
+                            <span>{ item.amount }</span>
+                            <p>{ item.title }</p>
+                        </div>
+                    ))}
+                </footer>
+            </main>
         </Container>
     )
 }
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,3 +1,85 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    height:100vh;
+    padding:4rem;
+    
+    main {
+        height:100%;
+
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+        justify-content:center;
+
+        > img {
+            width:16rem;
+            height:16rem;
+
+            object-fit:cover;
+            border-radius:50%;
+        }
+
+        > p {
+            font-size:1.8rem;
+            font-weight:bold;
+            
+            margin-top:1.8rem;
+        }
+
+        > span {
+            color:${({ theme }) => theme.colors.gray_800 };
+            margin-top:.2rem;
+        }
+
+        > .medias {
+            margin-top:1rem;
+
+            display:flex;
+            gap: 1rem;
+
+            > p {
+                color:${({ theme }) => theme.colors.gray_800 };
+
+                display:flex;
+                align-items:center;
+                gap:.4rem;
+            }
+        }
+
+        > footer {
+            margin-top:3rem;
+
+            display:flex;
+            align-items:center;
+            gap:1rem;
+
+            > div {
+                width:8rem;
+                height:8rem;
+
+                background-color:${({ theme }) => theme.colors.gray_300 };
+                border-radius:.5rem;
+
+                display:flex;
+                flex-direction:column;
+                align-items:center;
+                justify-content:center;
+
+                > span {
+                    color:${({ theme }) => theme.colors.purple_500 };
+
+                    font-weight:bold;
+                    font-size:1.2rem;
+                }
+
+                > p {
+                    color:${({ theme }) => theme.colors.gray_800 };
+                    text-align:center;
+
+                    margin-top:.8rem;
+                }
+            }
+        }
+    }
+`;
