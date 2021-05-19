c37d90cd94055dc170f1c00d022624494f948c5d
victorguirra <victor.hugo.guirra@gmail.com>
2021-05-19 20:22:22 -0300

    remove some items for aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Container, NavContainer, NavItem } from './styles';
 
 import { AiOutlinePlus, AiFillHeart } from 'react-icons/ai';
-import { MdSettings } from 'react-icons/md';
 import { BsPersonFill, BsSearch } from 'react-icons/bs';
-import { IoMdContact } from 'react-icons/io';
 
 function Aside(){
     return(
@@ -15,7 +13,7 @@ function Aside(){
             </div>
 
             <NavContainer>
-                <NavItem to="/">
+                <NavItem to="/petRegister">
                     <AiOutlinePlus size={ 25 } color="#FFF" />
                     Cadastrar Pet
                 </NavItem>
@@ -26,14 +24,6 @@ function Aside(){
                 <NavItem to="/main">
                     <BsSearch size={ 25 } color="#FFF" />
                     Encontrar Pets
-                </NavItem>
-                <NavItem to="/">
-                    <IoMdContact size={ 25 } color="#FFF" />
-                    Seus contatos
-                </NavItem>
-                <NavItem to="/settings">
-                    <MdSettings size={ 25 } color="#FFF" />
-                    Configurações
                 </NavItem>
                 <NavItem to="/profile">
                     <BsPersonFill size={ 25 } color="#FFF" />
